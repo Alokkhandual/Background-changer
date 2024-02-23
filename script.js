1e309999d58bc19312b2b0ae0b01a43fa2d6a6ce
AlokKhandual <alokkhandual45@gmail.com>
2024-02-23 19:49:40 +0530

    js project

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const black=document.querySelector('#btn1')
+black.addEventListener("click",function(){
+    document.body.style.backgroundColor="grey"
+})
+
+const white=document.querySelector("#btn2")
+white.addEventListener("click",function(){
+    document.body.style.backgroundColor="white"
+})
+
+const blue=document.querySelector("#btn3")
+blue.addEventListener("click",function(){
+    document.body.style.backgroundColor="blue"
+})
+
+const yellow=document.querySelector("#btn4")
+yellow.addEventListener("click",function(){
+    document.body.style.backgroundColor="yellow"
+})
